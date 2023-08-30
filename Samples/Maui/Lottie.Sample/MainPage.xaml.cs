ceb04c3ca75b9259f3785262b266112dc0ecd6b4
Martin Mareček <martin.marecek@solitea.cz>
2023-08-30 05:41:15 +0200

    android partialy ok

--- Samples/Maui/Lottie.Sample/MainPage.xaml.cs
+++ Samples/Maui/Lottie.Sample/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Lottie.Sample
+﻿using Microsoft.Maui.ApplicationModel;
+
+namespace Lottie.Sample
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,22 @@
         public MainPage()
         {
             InitializeComponent();
+            
+            //Task.Run(async () => 
+            //{
+            //    while (true)
+            //    {
+            //        await MainThread.InvokeOnMainThreadAsync(() =>
+            //        {
+            //            this.A1.enab
+            //            var old = this.A1.Progress;
+            //            this.A1.Progress = 0;
+            //            this.A1.Progress = old;
+            //        });
+
+            //        await Task.Delay(10);
+            //    }
+            //});            
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
